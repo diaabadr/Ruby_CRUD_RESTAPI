b68d6e7fa66b84a5f28d4248a7bd4ade87b245da
diaabadr <diaabadr82@gmail.com>
2023-04-20 12:18:26 +0200

    init + creating done

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::ArticlesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create 
+    article = Article.new(
+      title: arti_params[:title],
+      body: arti_params[:body],
+      author: arti_params[:author]
+    )
+    if article.save
+      render json:article, status: 201
+    else
+      render json: {
+        error: "Error creating article"
+      }
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def arti_params
+    params.require(:article).permit(:title, :body, :author)
+  end
+end
