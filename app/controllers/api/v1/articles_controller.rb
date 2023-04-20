7cc6c1f96d75c6df73e8a63051d39afade6d910f
diaabadr <diaabadr82@gmail.com>
2023-04-20 12:37:18 +0200

    Get, Update, Delete Done

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,8 +1,18 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
+    articles = Article.all
+    render json: articles, status:200
   end
 
   def show
+    article = Article.find_by(id: params[:id])
+    if article 
+      render json: article, status: 200
+    else
+      render json:{
+        error: "Article not found"
+      }
+    end
   end
 
   def create 
@@ -21,9 +31,31 @@ class Api::V1::ArticlesController < ApplicationController
   end
 
   def update
+    article = Article.find_by(id: params[:id])
+    if article
+      article.update(title: params[:title], body: params[:body], author: params[:author])
+      render json: article, status: 200
+    else
+      render json: {
+        error: "Article not found"
+      }
+    end
+    
   end
 
   def destroy
+    article = Article.find_by(id: params[:id])
+    if article
+      article.destroy
+      render json: {
+        message: "Article deleted"
+      }
+    else
+      render json: {
+        error: "Article not found"
+      }
+    end
+
   end
 
   private
